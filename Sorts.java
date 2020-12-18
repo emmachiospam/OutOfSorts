3c2d6c5e30792933a1e5d49a3fe7e9d90f6543f4
emmachio <emmany2409@gmail.com>
2020-12-17 20:04:37 -0500

    works

--- Sorts.java
+++ Sorts.java
@@ -3,18 +3,21 @@ public class Sorts{
 
   public static void bubbleSort(int[] data) {
     int last = data.length;
-    for(int i = 0; i < data.length-1; i++) {
-      int a = data[i];
-      int b = data[i+1];
-      if(a > b) {
-        data[i] = b;
-        data[i+1] = a;
+    for(int j = 0; j < data.length; j++){
+      for(int i = 0; i < last-1; i++) {
+        System.out.println(Arrays.toString(data));
+        int a = data[i];
+        int b = data[i+1];
+        if(a > b) {
+          data[i] = b;
+          data[i+1] = a;
+        }
       }
+    last--;
     }
   }
-
   public static void main(String[] args) {
-    int[] nums = {1,5,1,2,4,3};
+    int[] nums = {4,3,2,10,12,1,5,6};
     bubbleSort(nums);
     String test = Arrays.toString(nums);
     System.out.println(test);
