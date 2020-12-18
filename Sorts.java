dd305e0b63b8dc1507d1ef68eb8167080fbbad7d
emmachio <emmany2409@gmail.com>
2020-12-17 21:42:53 -0500

    tested with test

--- Sorts.java
+++ Sorts.java
@@ -16,11 +16,5 @@ public class Sorts{
     last--;
     }
   }
-  public static void main(String[] args) {
-    int[] nums = {4,3,2,10,12,1,5,6};
-    bubbleSort(nums);
-    String test = Arrays.toString(nums);
-    System.out.println(test);
-  }
 
 }
