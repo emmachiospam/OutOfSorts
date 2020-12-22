cb585b9320c187a30b455a25bbd87a1d3435be6c
emmachio <emmany2409@gmail.com>
2020-12-22 12:41:38 -0500

    selection

--- Sorts.java
+++ Sorts.java
@@ -17,4 +17,21 @@ public class Sorts{
     }
   }
 
+  public static void selectionSort(int[] data) {
+    for(int i = 0; i < data.length; i++) {
+      int og = data[i];
+      int min = data[i];
+      int minIndex = i;
+      for(int j = i; j < data.length; j++) {
+        if(data[j] < min) {
+          min = data[j];
+          minIndex = j;
+        }
+      }
+      data[i] = min;
+      data[minIndex] = og;
+      System.out.println(Arrays.toString(data));
+    }
+  }
+
 }
