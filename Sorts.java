ffc700855eb753af2e4af82c9317fc67f33669ad
emmachio <emmany2409@gmail.com>
2020-12-24 20:48:31 -0500

    got rid of prints

--- Sorts.java
+++ Sorts.java
@@ -5,7 +5,6 @@ public class Sorts{
     int last = data.length;
     for(int j = 0; j < data.length; j++){
       for(int i = 0; i < last-1; i++) {
-        System.out.println(Arrays.toString(data));
         int a = data[i];
         int b = data[i+1];
         if(a > b) {
@@ -30,7 +29,6 @@ public class Sorts{
       }
       data[i] = min;
       data[minIndex] = og;
-      System.out.println(Arrays.toString(data));
     }
   }
 
